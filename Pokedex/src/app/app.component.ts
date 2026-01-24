b694341d8e9a110b7eb7fd77de888b93793acdc0
mateusorlandini <m.orlandini@nextage.com.br>
2026-01-24 12:00:11 -0300

    adicionado header

--- Pokedex/src/app/app.component.ts
+++ Pokedex/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {PokemonCardComponent} from "./pokemon-card/pokemon-card.component";
+import {HeaderComponent} from "./header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [PokemonCardComponent],
+  imports: [PokemonCardComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
