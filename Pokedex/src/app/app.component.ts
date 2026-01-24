3681c678ab0c9bdbf2a2b167d454eaeb280ca0f0
mateusorlandini <m.orlandini@nextage.com.br>
2026-01-24 11:51:46 -0300

    inicio

---
+++ Pokedex/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import {PokemonCardComponent} from "./pokemon-card/pokemon-card.component";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [PokemonCardComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'Pokedex';
+}
