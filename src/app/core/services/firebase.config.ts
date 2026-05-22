6275ac70e75d77d159d5b0e4203ced438e48a581
mateusorlandini <mateus.orlandini17@gmail.com>
2026-05-22 15:10:25 -0300

    refactor: externalize Firebase configuration and improve environment handling
    - Moved Firebase configuration to environment files for better separation of concerns.
    - Added `.env.example` and `environment.example.ts` to demonstrate required environment variables.
    - Updated `firebase.config.ts` to use environment-based Firebase settings.
    - Configured environment file replacements in `angular.json` for production builds.

--- src/app/core/services/firebase.config.ts
+++ src/app/core/services/firebase.config.ts
@@ -2,16 +2,8 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyC7doFHHLpTHH9g_gmKO5jQInG30DpuL6I',
-  authDomain: 'pokedex-5a812.firebaseapp.com',
-  databaseURL: 'https://pokedex-5a812-default-rtdb.firebaseio.com',
-  projectId: 'pokedex-5a812',
-  storageBucket: 'pokedex-5a812.firebasestorage.app',
-  messagingSenderId: '370943961176',
-  appId: '1:370943961176:web:ffc6736d06dcf2971f822d',
-};
+import { environment } from '../../../environments/environment';
 
-export const firebaseApp = initializeApp(firebaseConfig);
+export const firebaseApp = initializeApp(environment.firebase);
 export const firebaseAuth = getAuth(firebaseApp);
 export const firebaseDatabase = getDatabase(firebaseApp);
